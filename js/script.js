8b6bde81b32691b1690b48369e578c13893310d1
Konstantin Kirillov <kirilloffkostya@gmail.com>
2020-10-19 11:56:36 +0300

    start project

---
+++ js/script.js
@@ -0,0 +1,50 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const tabheaderItems = document.querySelector('.tabheader__items'),
+        menuItem = document.querySelectorAll('.tabheader__item'),
+        arrTabcontent = document.querySelectorAll('.tabcontent');
+
+
+    function clearTabcontainer() {
+        arrTabcontent.forEach((item) => {
+            item.style.display = 'none';
+        });
+
+        menuItem.forEach((item, i) => {
+            item.classList.remove('tabheader__item_active');
+        });
+    };
+
+    function showTabcontainer(i) {
+        arrTabcontent[i].style.display = 'block';
+        menuItem[i].classList.add('tabheader__item_active');
+    };
+
+    clearTabcontainer();
+    showTabcontainer(0);
+
+    tabheaderItems.addEventListener('click', (event) => {
+
+        let targetElement = event.target;
+
+        if (targetElement && targetElement.matches('.tabheader__item')) {
+            clearTabcontainer();
+            menuItem.forEach((item, i) => {
+                if (item == targetElement) {
+                    showTabcontainer(i);
+                };
+            });
+
+        }
+
+    });
+
+
+
+
+
+
+
+
+
+});
