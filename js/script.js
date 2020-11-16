c57760517096bb643804d8637a7b0ef3b9d48bcb
Konstantin Kirillov <kirilloffkostya@gmail.com>
2020-11-16 16:29:19 +0300

    add work with classes

--- js/script.js
+++ js/script.js
@@ -117,7 +117,7 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
     modalForm.addEventListener('click', function (event) {
-        if ( event.target === modalElem ) {
+        if (event.target === modalElem) {
             closeModal(modalForm);
         }
     });
@@ -135,7 +135,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     function showModalByScroll() {
 
-        if ( window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight ) {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
             showModal();
             document.removeEventListener('scroll', showModalByScroll);
         }
@@ -143,19 +143,85 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     document.addEventListener('keydown', (event) => {
-        if ( event.code === 'Escape' && modalForm.classList.contains('show')) {
+        if (event.code === 'Escape' && modalForm.classList.contains('show')) {
             closeModal(modalForm);
         }
     });
 
-    const modalTimerId = setTimeout(() => {
+    /* const modalTimerId = setTimeout(() => {
 
         if ( !modalForm.classList.contains('show') ) {
             showModal();
         }
 
-    }, 6000);
+    }, 6000); */
 
     document.addEventListener('scroll', showModalByScroll);
+
+
+
+    //Использование классов для карточек
+    class MenuItem {
+
+        constructor(src, subtitle, descr, price, ...classes) {
+            this.src = src;
+            this.subtitle = subtitle;
+            this.descr = descr;
+            this.price = price;
+            this.classes = classes;
+        }
+        render(container) {
+
+            const element = document.createElement('div');
+
+            if ( this.classes.length > 0 ) {
+                this.classes.forEach(className => element.classList.add(className));
+            } else {
+                element.classList.add("menu__item");
+            };
+
+
+            element.innerHTML +=
+            `<img src=${this.src} alt="vegy">
+            <h3 class="menu__item-subtitle">${this.subtitle}</h3>
+            <div class="menu__item-descr">${this.descr}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+                <div class="menu__item-cost">Цена:</div>
+                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+            </div>`;
+
+            container.append(element);
+        }
+    };
+
+    const menuItemContainer = document.querySelector('.menu__field .container');
+
+    //vegy
+    new MenuItem(
+        "img/tabs/vegy.jpg",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        229,
+        "menu__item",
+        "big").render(menuItemContainer);
+
+
+    //elite
+    new MenuItem(
+        "img/tabs/elite.jpg",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        550,
+        "menu__item").render(menuItemContainer);
+
+
+    //post
+    new MenuItem(
+        "img/tabs/post.jpg",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        430,
+        "menu__item").render(menuItemContainer);
 
 });
